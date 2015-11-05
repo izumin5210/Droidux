78e2284aec1a7967086e6961e69d087b04295665
izumin5210 <masayuki@izumin.info>
2015-11-05 21:41:21 +0900

    Refactor sample app

--- app/src/main/java/info/izumin/android/droidux/sample/MainActivity.java
+++ app/src/main/java/info/izumin/android/droidux/sample/MainActivity.java
@@ -2,82 +2,25 @@ package info.izumin.android.droidux.sample;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import info.izumin.android.droidux.sample.action.AddTodoAction;
-import info.izumin.android.droidux.sample.action.ClearCompletedTodoAction;
-import info.izumin.android.droidux.sample.action.DeleteTodoAction;
-import info.izumin.android.droidux.sample.action.ToggleCompletedTodoAction;
-import info.izumin.android.droidux.sample.databinding.ActivityMainBinding;
 import info.izumin.android.droidux.sample.reducer.DroiduxRootStore;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.subjects.PublishSubject;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
 
-    private EditText editNewTodo;
-    private Button btnAddTodo;
-    private ListView listTodo;
-
     private DroiduxRootStore store;
+    private MainActivityHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        DataBindingUtil.setContentView(this, R.layout.activity_main);
         store = ((App) getApplication()).getStore();
-
-        editNewTodo = (EditText) findViewById(R.id.edit_new_todo);
-        btnAddTodo = (Button) findViewById(R.id.btn_add_todo);
-        listTodo = (ListView) findViewById(R.id.list_todo);
-
-        PublishSubject<String> btnAddTodoSubject = PublishSubject.create();
-        btnAddTodo.setOnClickListener(v -> btnAddTodoSubject.onNext(editNewTodo.getText().toString()));
-        btnAddTodoSubject.filter(s -> !s.isEmpty())
-                .flatMap(s -> store.dispatch(new AddTodoAction(s)))
-                .subscribeOn(AndroidSchedulers.mainThread())
-                .subscribe(action -> {
-                    editNewTodo.setText("");
-                    Toast.makeText(MainActivity.this, R.string.toast_add_todo, Toast.LENGTH_SHORT).show();
-                });
-
-        PublishSubject<Long> listTodoItemClickSubject = PublishSubject.create();
-        listTodo.setOnItemClickListener((parent, view, position, id) -> listTodoItemClickSubject.onNext(id));
-        listTodoItemClickSubject
-                .flatMap(id -> store.dispatch(new ToggleCompletedTodoAction(id.intValue())))
-                .subscribe();
-
-        PublishSubject<Long> listTodoItemLongClickSubject = PublishSubject.create();
-        listTodo.setOnItemLongClickListener((parent, view, position, id) -> {
-            new AlertDialog.Builder(this)
-                    .setTitle(R.string.dialog_delete_todo_title)
-                    .setMessage(getString(R.string.dialog_delete_todo_message,
-                            store.getTodoListStore().getState().getTodoById((int) id).getText()))
-                    .setPositiveButton(R.string.dialog_delete_todo_btn_positive, (dialog, which) -> {
-                        listTodoItemLongClickSubject.onNext(id);
-                    })
-                    .setNeutralButton(R.string.dialog_delete_todo_btn_neutral, (dialog, which) -> {
-                        dialog.dismiss();
-                    })
-                    .show();
-            return true;
-        });
-        listTodoItemLongClickSubject
-                .flatMap(id -> store.dispatch(new DeleteTodoAction(id)))
-                .subscribeOn(AndroidSchedulers.mainThread())
-                .subscribe(action -> {
-                    Toast.makeText(this, R.string.toast_delete_todo, Toast.LENGTH_SHORT).show();
-                });
-
-        listTodo.setAdapter(new TodoListAdapter(this));
+        helper = new MainActivityHelper(this);
+        helper.onCreate();
     }
 
     @Override
@@ -88,12 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_clear_completed_todo:
-                store.dispatch(new ClearCompletedTodoAction()).subscribe();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+        return helper.onOptionItemSelected(item) || super.onOptionsItemSelected(item);
     }
 }
