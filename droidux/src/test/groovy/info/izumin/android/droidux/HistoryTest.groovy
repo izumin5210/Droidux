822f3ac6e6997442323e1da77ddf0dd36602027c
izumin5210 <masayuki@izumin.info>
2015-11-24 08:28:18 +0900

    Fix broken test

--- droidux/src/test/groovy/info/izumin/android/droidux/HistoryTest.groovy
+++ droidux/src/test/groovy/info/izumin/android/droidux/HistoryTest.groovy
@@ -3,7 +3,7 @@ package info.izumin.android.droidux
 import spock.lang.Specification;
 
 /**
- * Created by izumin on 11/24/15.
+ * Created by izumin on new Counter(1)new Counter(1)/new Counter(2)new Counter(4)/new Counter(1)new Counter(5).
  */
 class HistoryTest extends Specification {
 
@@ -11,8 +11,31 @@ class HistoryTest extends Specification {
     def past
     def future
 
+    static class Counter implements UndoableState<Counter> {
+        def count
+
+        Counter(count) {
+            this.count = count
+        }
+
+        @Override
+        boolean equals(Object o) {
+            return (o instanceof Counter) && (o.hashCode() == hashCode())
+        }
+
+        @Override
+        int hashCode() {
+            return count
+        }
+
+        @Override
+        Counter clone() {
+            return new Counter(count)
+        }
+    }
+
     def setup() {
-        history = new History<Integer>(0)
+        history = new History<Counter>(new Counter(0))
         past = History.metaClass.getAttribute(history, "past")
         future = History.metaClass.getAttribute(history, "future")
     }
@@ -23,104 +46,104 @@ class HistoryTest extends Specification {
 
     def "#insert()"() {
         when:
-        history.insert(1)
+        history.insert(new Counter(1))
 
         then:
-        getPresent() == 1
+        getPresent() == new Counter(1)
 
         when:
-        history.insert(3)
+        history.insert(new Counter(3))
 
         then:
-        getPresent() == 3
+        getPresent() == new Counter(3)
     }
 
     def "#undo()"() {
         when:
-        history.insert(1)
+        history.insert(new Counter(1))
 
         then:
         past.size() == 1
-        history.undo() == 0
+        history.undo() == new Counter(0)
         past.size() == 0
 
         when:
-        history.insert(3)
-        history.insert(5)
+        history.insert(new Counter(3))
+        history.insert(new Counter(5))
 
         then:
         past.size() == 2
-        history.undo() == 3
+        history.undo() == new Counter(3)
         past.size() == 1
-        history.undo() == 0
+        history.undo() == new Counter(0)
         past.size() == 0
-        history.undo() == 0
+        history.undo() == new Counter(0)
     }
 
     def "#redo()"() {
         when:
-        history.insert(1)
+        history.insert(new Counter(1))
         history.undo()
 
         then:
         future.size() == 1
-        history.redo() == 1
+        history.redo() == new Counter(1)
         future.size() == 0
 
         when:
-        history.insert(3)
-        history.insert(5)
+        history.insert(new Counter(3))
+        history.insert(new Counter(5))
         history.undo()
         history.undo()
 
         then:
         future.size() == 2
-        history.redo() == 3
+        history.redo() == new Counter(3)
         future.size() == 1
-        history.redo() == 5
+        history.redo() == new Counter(5)
         future.size() == 0
-        history.redo() == 5
+        history.redo() == new Counter(5)
 
         when:
-        history.insert(8)
-        history.insert(7)
+        history.insert(new Counter(8))
+        history.insert(new Counter(7))
         history.undo()
         history.undo()
 
         then:
         future.size() == 2
-        history.insert(4)
+        history.insert(new Counter(4))
         future.size() == 0
     }
 
     def "#setLimit()"() {
         when:
         history.setLimit(3)
-        history.insert(1)
+        history.insert(new Counter(1))
 
         then:
         past.size() == 1
 
         when:
-        history.insert(2)
+        history.insert(new Counter(2))
 
         then:
         past.size() == 2
 
         when:
-        history.insert(3)
+        history.insert(new Counter(3))
 
         then:
         past.size() == 3
 
         when:
-        history.insert(4)
+        history.insert(new Counter(4))
 
         then:
         past.size() == 3
-        history.undo() == 3
-        history.undo() == 2
-        history.undo() == 1
-        history.undo() == 1
+        history.undo() == new Counter(3)
+        history.undo() == new Counter(2)
+        history.undo() == new Counter(1)
+        history.undo() == new Counter(1)
     }
 }
