3c75c2fbb02eb0109676f7500fab952787067470
izumin5210 <masayuki@izumin.info>
2015-11-29 02:34:40 +0900

    Rename InvalidStateClassException -> InvalidStoreDeclarationException

---
+++ droidux-processor/src/main/java/info/izumin/android/droidux/processor/exception/InvalidStoreDelcarationException.java
@@ -0,0 +1,10 @@
+package info.izumin.android.droidux.processor.exception;
+
+/**
+ * Created by izumin on 11/24/15.
+ */
+public class InvalidStoreDelcarationException extends RuntimeException {
+    public InvalidStoreDelcarationException(String message) {
+        super(message);
+    }
+}
