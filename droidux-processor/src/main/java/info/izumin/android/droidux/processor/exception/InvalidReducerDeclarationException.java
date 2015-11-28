95ba450ede75146e950a7a4c84aea8b056f2dd0a
izumin5210 <masayuki@izumin.info>
2015-11-29 02:17:23 +0900

    Rename InvalidClassNameException -> InvalidReducerDeclarationException

---
+++ droidux-processor/src/main/java/info/izumin/android/droidux/processor/exception/InvalidReducerDeclarationException.java
@@ -0,0 +1,10 @@
+package info.izumin.android.droidux.processor.exception;
+
+/**
+ * Created by izumin on 11/8/15.
+ */
+public class InvalidReducerDeclarationException extends RuntimeException {
+    public InvalidReducerDeclarationException(String message) {
+        super(message);
+    }
+}
