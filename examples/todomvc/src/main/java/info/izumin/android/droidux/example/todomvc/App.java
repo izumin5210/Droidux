a1207da8a16f126a17443bc8caa8c6538b19fb9d
izumin5210 <masayuki@izumin.info>
2015-11-29 23:47:01 +0900

    Fix samples

--- examples/todomvc/src/main/java/info/izumin/android/droidux/example/todomvc/App.java
+++ examples/todomvc/src/main/java/info/izumin/android/droidux/example/todomvc/App.java
@@ -19,7 +19,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        store = new DroiduxRootStore.Builder()
+        store = DroiduxRootStore.builder()
                 .addMiddleware(new Logger())
                 .setReducer(new TodoListReducer())
                 .setInitialState(new TodoList(new ArrayList<>()))
