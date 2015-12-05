f01d071dcae0041fd5c21a15821f2bbccd2bdb1c
izumin5210 <masayuki@izumin.info>
2015-12-05 21:51:44 +0900

    Fix constructor params of {Un,Re}doAction

--- droidux/src/main/java/info/izumin/android/droidux/action/RedoAction.java
+++ droidux/src/main/java/info/izumin/android/droidux/action/RedoAction.java
@@ -1,14 +1,12 @@
 package info.izumin.android.droidux.action;
 
-import info.izumin.android.droidux.UndoableState;
-
 /**
  * Created by izumin on 11/24/15.
  */
 public class RedoAction extends HistoryAction {
     public static final String TAG = RedoAction.class.getSimpleName();
 
-    public RedoAction(Class<? extends UndoableState> targetStateType) {
-        super(Kind.REDO, targetStateType);
+    public RedoAction(Class targetReducerType) {
+        super(Kind.REDO, targetReducerType);
     }
 }
--- droidux/src/main/java/info/izumin/android/droidux/action/UndoAction.java
+++ droidux/src/main/java/info/izumin/android/droidux/action/UndoAction.java
@@ -1,14 +1,12 @@
 package info.izumin.android.droidux.action;
 
-import info.izumin.android.droidux.UndoableState;
-
 /**
  * Created by izumin on 11/24/15.
  */
 public class UndoAction extends HistoryAction {
     public static final String TAG = UndoAction.class.getSimpleName();
 
-    public UndoAction(Class<? extends UndoableState> targetStateType) {
-        super(Kind.UNDO, targetStateType);
+    public UndoAction(Class targetReducerType) {
+        super(Kind.UNDO, targetReducerType);
     }
 }
